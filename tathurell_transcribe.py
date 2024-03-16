1028bbd3b5f5062dceffcade6ba804b06ffb9d98
pksebben <ben@sentenai.com>
2024-03-16 15:35:02 -0700

    implement rough diarization

--- tathurell_transcribe.py
+++ tathurell_transcribe.py
@@ -32,6 +32,8 @@ chunk_duration = 4000 / 16000
 # Total duration of all processed chunks
 total_duration = 0
 
+combined_transcription = []
+
 # Process chunks of the audio data
 print("PROCESSING")
 for i in range(0, len(wav_data), 4000):
@@ -42,6 +44,9 @@ for i in range(0, len(wav_data), 4000):
         # Adjust each word's timestamps
         if "result" in res:
             for word in res.get("result", []):
+                combined_transcription.append(
+                    {"word": word["word"], "start": word["start"]}
+                )
                 word["start"] += total_duration
                 word["end"] += total_duration
             print(res)
@@ -51,9 +56,61 @@ for i in range(0, len(wav_data), 4000):
 # Get the final result without timestamps
 res = json.loads(rec.FinalResult())
 
-timestamp = read_timestamps_from_metadata(audio_path)
-print(f"Recording started at {timestamp[0]}")
-for word in res["result"]:
+# timestamp = read_timestamps_from_metadata(audio_path)
+# print(f"Recording started at {timestamp[0]}")
+for word in combined_transcription:
     print(f"{word['start']}: {word['word']}")
 
-# print(res)
+print(
+    """
+
+
+TRANSCRIPTION COMPLETE.
+
+>>STARTING SPEAKER DIATRIZATION>>
+
+"""
+)
+from pyannote.audio import Pipeline
+
+pipeline = Pipeline.from_pretrained(
+    "pyannote/speaker-diarization-3.1",
+    use_auth_token="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",
+)
+
+# send pipeline to GPU (when available)
+import torch
+import torchaudio
+
+pipeline.to(torch.device("mps"))
+
+# apply pretrained pipeline
+waveform, sample_rate = torchaudio.load(audio_path)
+diarization = pipeline({"waveform": waveform, "sample_rate": sample_rate})
+
+current_speaker = None
+current_chunk = ""
+diatrized_transcription = []
+
+# print the result
+for turn, _, speaker in diarization.itertracks(yield_label=True):
+    if speaker != current_speaker:
+        if current_speaker is not None:
+            diatrized_transcription.append(
+                {"speaker": current_speaker, "text": current_chunk}
+            )
+        current_chunk = ""
+        current_speaker = speaker
+    for word in combined_transcription:
+        if word["start"] < turn.start:
+            pass
+        elif word["start"] > turn.end:
+            pass
+        else:
+            current_chunk += f" {word['word']}"
+    print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
+
+
+# for entry in diatrized_transcription:
+# print("")
+# print(f"{entry['speaker']}:: {entry['text']}")
